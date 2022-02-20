5ce751685cc22c57abad339c0910dbe63f7fb372
Hadi Amjad <46374292+hadiamjad@users.noreply.github.com>
2022-02-20 10:09:31 -0500

    updated relationships

--- analysis.py
+++ analysis.py
@@ -24,9 +24,9 @@ def addNode(nodes, name, type, TC, FC, classlabel):
     nodes[name][3] += FC
   return nodes[name][0]
 
-def addEdge(edges, src, tar):
+def addEdge(edges, src, tar, type):
   if str(src)+'@'+str(tar) not in edges.keys():
-    edges[str(src)+'@'+str(tar)] = [src, tar]
+    edges[str(src)+'@'+str(tar)] = [src, tar, type]
 
 
 # storage_dic = {'_gid' = [002, 5288992, 1], '_svd' = [5]}
@@ -306,14 +306,21 @@ def IsInfoShared(storage_dic, url):
 
 def createWebGraph(url):
     
-    # name: [id, type, label, ]
+    # name: [id, type, TC, FC, label]
     nodes = {}
-    # src@tar = [src_id, tar_id]
+    # src@tar = [src_id, tar_id, type]
     edges = {}
     # script_dic = {'https://ad/test.js': [set->[_gid,..], get->[_svd, ..]]}
     script_dic = {} 
     # storage_dic = {'_gid' = [002, 5288992, 1], '_svd' = [5]}
     storage_dic = {}
+
+    # initial HTML iframe
+    src = addNode(nodes, "Network@https://"+url+"/", "Network", 0 , 0, -1)
+    tar = addNode(nodes, "HTML@https://"+url+"/", "HTML@iframe", 0 , 0, -2)
+    addEdge(edges, src, tar, 'Network->HTML/Script')
+    src = addNode(nodes, "Script@https://"+url+"/", "Script", 0 , 0, 0)
+    addEdge(edges, tar, src, 'Initiated')
 
     # creating storage nodes and edges
     with open(r'server/cookie_storage.json') as file:
@@ -324,7 +331,6 @@ def createWebGraph(url):
     for key in storage_dic:
       addNode(nodes, "Storage@"+key, "Storage", 0 , 0, -3)
     
-    print(nodes)
     # reading big request data line by line
     with open(r'labellings.json') as file:
       for line in file:
@@ -340,7 +346,7 @@ def createWebGraph(url):
             rdurl = getRedirection(dataset["request_id"], dataset["http_req"])
             if rdurl is not None:
               tar = addNode(nodes, "Network@"+rdurl, "Network", 0 , 0, -1)
-              addEdge(edges, src, tar)
+              addEdge(edges, src, tar, 'Redirection')
             
             # check if resource type is not script then create simple HTML node
             if dataset["resource_type"] != "Script":
@@ -349,35 +355,71 @@ def createWebGraph(url):
             else:
               tar = addNode(nodes, "Script@"+dataset["http_req"], dataset["resource_type"], 0, 0, 0)
             # create edge between the Request -> HTML/Script
-            addEdge(edges, src, tar)
+            addEdge(edges, src, tar, 'Network->HTML/Script')
             
             # if its initiated by call stack javascript
+            # else its generated from main iframe
             if dataset['call_stack']['type'] == 'script':
               if dataset['easylistflag'] == 1 or dataset['easyprivacylistflag'] == 1 or dataset['ancestorflag'] == 1:
                 tar = addNode(nodes, "Script@"+getInitiator(dataset['call_stack']['stack']), "Script", 1, 0, 0)
               else:
                 tar = addNode(nodes, "Script@"+getInitiator(dataset['call_stack']['stack']), "Script", 0, 1, 0)
-              addEdge(edges, tar, src)
+              addEdge(edges, tar, src, 'Initiated')
+            else:
+              addEdge(edges, nodes["HTML@https://www."+url+"/"][0], src, 'Initiated')
             
             # Links between storage nodes and script [setter, getter]
             if dataset["http_req"] in script_dic.keys():
               # script -> setter 
               if len(script_dic[dataset["http_req"]][0]) != 0:
                  for item in script_dic[dataset["http_req"]][0]:
-                   addEdge(edges, src, nodes['Storage@'+item][0])
+                   addEdge(edges, nodes['Script@'+dataset["http_req"]][0], nodes['Storage@'+item][0], 'Storage Setter')
               # getter -> script
               if len(script_dic[dataset["http_req"]][1]) != 0:
                  for item in script_dic[dataset["http_req"]][1]:
-                   addEdge(edges, nodes['Storage@'+item][0], src)
+                   addEdge(edges, nodes['Storage@'+item][0], nodes['Script@'+dataset["http_req"]][0], 'Storage Getter')
 
             # if url has storage info 
             val = IsInfoShared(storage_dic, dataset["http_req"])
             if val is not None:
-              addEdge(edges, nodes['Storage@'+val][0], src)
+              addEdge(edges, nodes['Storage@'+val][0], src, 'Info Shared')
     
     print(nodes)
     print(edges)
-          
+
+    plot = Digraph(comment='The Round Table')
+
+    for key in nodes:
+      
+      if nodes[key][1] == 'Script':
+        if nodes[key][2] == 0:
+          plot.node(str(nodes[key][0]), str(nodes[key][0]), color='green', style='filled')
+        elif nodes[key][3] == 0 and nodes[key][2] != 0:
+          plot.node(str(nodes[key][0]), str(nodes[key][0]), color='red', style='filled')
+        else:
+          plot.node(str(nodes[key][0]), str(nodes[key][0]), color='yellow', style='filled')
+      elif nodes[key][4] == -3:
+        plot.node(str(nodes[key][0]), str(nodes[key][0]), color='blue', style='filled')
+      elif nodes[key][4] == -2:
+        plot.node(str(nodes[key][0]),str(nodes[key][0]), color='orange', style='filled')
+      else:
+        plot.node(str(nodes[key][0]), str(nodes[key][0]), color='purple', style='filled')
+    
+    for key in edges:
+      if edges[key][2] == 'Network->HTML/Script':
+        plot.edge(str(edges[key][0]), str(edges[key][1]), arrowhead='normal')
+      elif edges[key][2] == 'Info Shared':
+        plot.edge(str(edges[key][0]), str(edges[key][1]), arrowhead='diamond')
+      elif edges[key][2] == 'Initiated':
+        plot.edge(str(edges[key][0]), str(edges[key][1]), arrowhead='tee')
+      elif edges[key][2] == 'Redirection':
+        plot.edge(str(edges[key][0]), str(edges[key][1]), arrowhead='halfopen')
+      elif edges[key][2] == 'Storage Getter':
+        plot.edge(str(edges[key][0]), str(edges[key][1]), arrowhead='crow')
+      else:
+        plot.edge(str(edges[key][0]), str(edges[key][1]), arrowhead='crow')
+    
+    plot.render('test-output/arabic.json.gv', view=True)
 
 
 def main2():
